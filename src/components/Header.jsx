1a12382828636c1f5e843a094afd10ab4d125fa2
Toshi <21s1328@andrew.ac.jp>
2023-11-10 13:40:49 +0900

    ヘッダーの追加とpageの追加

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+// Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; 
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <img src="/path-to-your-logo.png" alt="Website Logo" />
+        </Link>
+      </div>
+      <nav className="navigation">
+        <ul>
+          <li><Link to="/">ホーム</Link></li>
+          <li><Link to="/news">お知らせ</Link></li>
+          <li><Link to="/project">企画一覧</Link></li>
+          <li><Link to="/access">アクセス</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Access.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Access() {
+  return (
+    <div>
+      <h1>アクセス情報</h1>
+    </div>
+  );
+}
+
+export default Access;
