6e5e1b1c32e3a8750f63b7e3130c1fee817f7b2d
fukai0116 <134059797+fukai0116@users.noreply.github.com>
2023-11-18 01:30:28 +0900

    ヘッダーにハンバーガーメニュー（笑）を追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-// Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import './Header.css'; 
 
 function Header() {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
   return (
     <header className="header">
       <div className="logo">
@@ -11,14 +14,16 @@ function Header() {
           <img src="momosai02.png" alt="桃山祭" />
         </Link>
       </div>
-      <nav className="navigation">
+      <button className="nav-toggle" onClick={() => setIsNavVisible(!isNavVisible)}>
+        <FontAwesomeIcon icon={isNavVisible ? faCircleXmark : faBars} />
+      </button>
+      <nav className={`navigation ${isNavVisible ? 'navigation-visible' : ''}`}>
         <ul>
-          <li><Link to="/">ホーム</Link></li>
-          <li><Link to="/news">お知らせ</Link></li>
-          <li><Link to="/project">企画一覧</Link></li>
-          <li><Link to="/access">アクセス</Link></li>
-          <li><Link to="/Notes">注意事項</Link></li>
-
+          <li><Link to="/" onClick={() => setIsNavVisible(false)}>ホーム</Link></li>
+          <li><Link to="/news" onClick={() => setIsNavVisible(false)}>お知らせ</Link></li>
+          <li><Link to="/project" onClick={() => setIsNavVisible(false)}>企画一覧</Link></li>
+          <li><Link to="/access" onClick={() => setIsNavVisible(false)}>アクセス</Link></li>
+          <li><Link to="/Notes" onClick={() => setIsNavVisible(false)}>注意事項</Link></li>
         </ul>
       </nav>
     </header>
