a27e3ee9ebc03c5615a4a8a82407fa6d16c9fd67
Makimaki1304 <23e1304@andrew.ac.jp>
2023-11-22 04:46:40 +0900

    twitterを追加

--- src/components/Newss.jsx
+++ src/components/Newss.jsx
@@ -5,7 +5,10 @@ const News = () => {
   const [notifications] = useState([
     { id: 1, content: "桃山祭まであと2日!", tags: ["桃山祭2023"], image: "midori.png", date: new Date("2023-11-22") },
     { id: 2, content: "桃山祭まであと4日!", tags: ["桃山祭2023"], image: "pan.png", date: new Date("2023-11-20") },
-    { id: 3, content: "桃山祭まであと6日!", tags: ["桃山祭2023"], image: "pan2.png", date: new Date("2023-11-18") },
+    { id: 3, content: "桃山祭当日限定！テック部ホットサンド販売!", tags: ["桃山祭2023","テック部","模擬店"], image: "kuso.png", date: new Date("2023-11-19")},
+    { id: 4, content: "桃山祭まであと6日!", tags: ["桃山祭2023"], image: "pan2.png", date: new Date("2023-11-18") },
+    { id: 5, content: "桃山祭当日限定! モモンガP新作ゲーム!", tags: ["桃山祭2023","モモンガP","ゲーム"], image: "momosai2.png", date: new Date("2023-11-18") },
+    { id: 6, content: "桃山祭当日でもテック部は新入部員募集中!", tags: ["桃山祭2023","テック部"], image: "teck.png", date: new Date("2023-11-13")},
     // ここに必要なだけお知らせを追加
   ]);
 
